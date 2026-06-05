a5bd0139ed0bd0ed15a643338060904cc742de9d
AndraCristiana07 <taporandra@gmail.com>
2026-06-05 21:07:27 +0100

    connected to kubernetes and print allpods

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"backend/config"
 	"backend/controllers"
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	// initialize Database
 	config.ConnectDatabase()
+
+	fmt.Println("Scanning Kubernetes cluster for pods...")
+	watchPods()
 
 	// setup Gin Router
 	r := gin.Default()
@@ -23,4 +33,35 @@ func main() {
 
 	// start Server on port 8080
 	r.Run(":8080")
+}
+
+func watchPods() {
+	kubeconfig := filepath.Join(homeDir(), ".kube", "config")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Found %d pods:\n", len(pods.Items))
+	for _, pod := range pods.Items {
+		fmt.Printf("- [%s] %s\n", pod.Namespace, pod.Name)
+	}
+}
+
+func homeDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic("could not determine home directory")
+	}
+	return home
 }
