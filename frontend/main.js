1baff40e2e917b50dcf9d10b399b35349a94557a
AndraCristiana07 <taporandra@gmail.com>
2026-06-05 23:32:20 +0100

    basic electron window

---
+++ frontend/main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+  });
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
